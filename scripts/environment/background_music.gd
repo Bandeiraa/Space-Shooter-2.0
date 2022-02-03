b5a9983389b82c5f7bae93e4e363d1e86a08ab9f
davi bandeira <fcodavibandeira@edu.unifor.br>
2022-02-03 09:11:55 -0300

    Background Music

---
+++ scripts/environment/background_music.gd
@@ -0,0 +1,20 @@
+extends AudioStreamPlayer
+class_name BGM
+
+var sound_list: Dictionary = {
+	"Between Worlds":"res://assets/background_music/between_worlds.ogg",
+	"Endless Adventure":"res://assets/background_music/endless_adventure.ogg",
+	"Galaxy Exploration" :"res://assets/background_music/galaxy_exploration.ogg",
+	"Lost in Space" :"res://assets/background_music/lost_in_space.ogg"
+}
+
+func _ready() -> void:
+	randomize()
+	var keys: Array = sound_list.keys()
+	var selected_key: String = keys[get_random_number()]
+	stream = load(sound_list[selected_key])
+	play()
+	
+	
+func get_random_number() -> int:
+	return randi() % sound_list.size()
